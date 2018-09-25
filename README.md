7b35e6f23f218e585b370b12887eb6f19e331190
Alfiyanm <muchlas12@gmail.com>
2018-09-25 16:29:11 +0700

    inheritance

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
  Pembelajaran OOP dalam PHP
 
  Author : muchlas12@gmail.com
+ github : <https://www.github.com/Alfiyanm>
 
  list of docs:
 1. object & class
@@ -11,6 +12,7 @@
 4. magic method & magic constant
 5. encapsulation
 6. setter & getter
+7. Inheritance
 
  #--- Steve McConnel ---#
  Suatu aplikasi yang besar (kompleks) didesain sedemikian rupa, sehingga kompleksitasnya dibagi menjadi unit-unit kecil yang lebih mudah dan dapat dikerjakan.
---
+++ inheritance.php
@@ -0,0 +1,124 @@
+<?php
+
+    /**
+     * --------------------- Inheritance --------------------------
+     *      - Inheritance adalah kemampuan untuk mengurangi duplikasi kode program.
+     *      - secara bahasa dikenal juga dengan 'pewarisan'.
+     *      - untuk membuat inheritance, atau class yang menurun dari class lainnya,
+     *        kita menggunakan keyword 'extends'
+     * 
+     */
+
+     // pola inheritance seperti dibawah ini ...
+    class Induk {
+
+        //kode lainnya
+    }
+
+    class Anak extends Induk {
+
+        //Kode lainnya
+    }
+
+    //------------------------- Membuat Turunan Class ----------------------------------
+
+    // Parent Class
+    class Mobil {
+
+        private $merk;
+
+        public function setMerk($merk)
+        {
+            $this->merk = $merk;
+        }
+
+        public function getMerk()
+        {
+            return $this->merk;
+        }
+
+        public function halo()
+        {
+            return 'Tin...tin... Halo saya adalah ' . $this->merk;
+        }
+    }
+
+    // Child Class
+    class MobilSport extends Mobil {}
+    
+    // Instance MobilSport
+    $ferrari = new MobilSport();
+    $ferrari->setMerk('Ferrari');
+    echo $ferrari->halo();
+
+    /**
+     * ----------------- Menambah Property & Method di Child Class -----------
+     * 
+     */
+
+    // Parent Class
+    class Motor {
+
+        private $merk;
+
+        public function setMerk($merk)
+        {
+            $this->merk = $merk;
+        }
+
+        public function getMerk()
+        {
+            return $this->merk;
+        }
+
+        public function halo()
+        {
+            return 'Tin...tin... Halo saya adalah ' .$this->merk;
+        }
+    }
+
+    // Child Class
+    class MotorSport extends Motor {
+
+        private $nos = 'Nitrous Oxide Systems';
+
+        public function ngebut()
+        {
+            return 'Mengendarai ' . $this->getMerk() . ' dengan ' . $this->nos;
+        }
+    }
+
+    // Inheritance MotorSport
+    $r1m = new MotorSport();
+    $r1m->setMerk('R1M');
+    echo $r1m->ngebut();
+
+    /**
+     * --------- Protected Access Modifier ----------------------
+     * 
+     */
+
+     // Parent Class
+     class Kapal {
+
+        protected $merk;
+
+        public function setMerk($merk)
+        {
+            $this->merk = $merk;
+        }
+     }
+
+     // Child Class
+     class KapalPesiar extends Kapal {
+
+        public function halo()
+        {
+            return 'Halo saya ' . $this->merk;
+        }
+     }
+
+     // Instance KapalPesiar
+     $dreamCruise = new KapalPesiar();
+     $dreamCruise->setMerk('Dream Cruise');
+     echo $dreamCruise->halo();
